9184d68a4e9222b846444c96c48d398f2a50f368
Giacomo Aloisi <giacomo.aloisi1998@gmail.com>
2021-07-30 00:28:07 +0200

    Add loop in client to send a request every second

--- rpctest-client/src/main.rs
+++ rpctest-client/src/main.rs
@@ -5,16 +5,25 @@ use protos::worker_service::*;
 use tonic;
 use tokio;
 
+use std::{thread, time};
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let mut client = WorkerServiceClient::connect("http://[::1]:50051").await?;
+    let mut client = WorkerServiceClient::connect("http://rpc-server:50051").await?;
+    let mut index = 1u32;
 
-    let request = tonic::Request::new(WorkerRequest::default());
+    loop {
+        let request = tonic::Request::new(TestRequest{ name: "Request #".to_owned() + &index.to_string() });
 
-    println!("Sending request to gRPC Server...");
-    let response = client.evaluate_submission(request).await?;
+        println!("Sending request to gRPC Server...");
+        let response = client.test_worker(request).await?;
 
-    println!("RESPONSE={:?}", response);
+        println!("RESPONSE={:?}", response);
+
+        index += 1;
+
+        thread::sleep(time::Duration::from_secs(1));
+    }
 
     Ok(())
 }
