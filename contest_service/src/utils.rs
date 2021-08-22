5543ae0ae53b29c1540ffb551c6a061097caa470
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-08-22 10:51:31 +0200

    Documented utils

--- contest_service/src/utils.rs
+++ contest_service/src/utils.rs
@@ -3,6 +3,7 @@ pub fn timestamp_to_systime(ts: mongodb::bson::Timestamp) -> std::time::SystemTi
     std::time::UNIX_EPOCH + std::time::Duration::from_secs(ts.time.into())
 }
 
+/// Convert a SystemTime to a protobuf timestamp. Not sure about the correctness of nanoseconds calculation
 pub fn systime_to_prost_ts(t: std::time::SystemTime) -> protos::prost_types::Timestamp {
     let nano_duration = t.duration_since(std::time::UNIX_EPOCH).unwrap().as_nanos();
     let seconds = (nano_duration / 1_000_000_000) as i64;
@@ -10,11 +11,13 @@ pub fn systime_to_prost_ts(t: std::time::SystemTime) -> protos::prost_types::Tim
     protos::prost_types::Timestamp { seconds, nanos }
 }
 
+/// Convert a protobuf timestamp to a Rust SystemTime
 pub fn prost_ts_to_systime(t: protos::prost_types::Timestamp) -> std::time::SystemTime {
     let nano_duration = t.nanos as u64 + t.seconds as u64 * 1_000_000_000;
     std::time::UNIX_EPOCH + std::time::Duration::from_nanos(nano_duration)
 }
 
+/// Convert a SystemTime to a MongoDB timestamp. Note that MongoDB sucks and stores unix timestamps in 32 bits
 pub fn systime_to_timestamp(st: std::time::SystemTime) -> mongodb::bson::Timestamp {
     let nano_duration = st.duration_since(std::time::UNIX_EPOCH).unwrap().as_nanos();
     let seconds = (nano_duration / 1_000_000_000) as u32; // TODO This is bad, because we will have an overflow on 2032 or such
