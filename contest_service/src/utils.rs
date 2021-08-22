8dbea851cc5e2ce9098b2e718774e1e45ed3cef9
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-08-22 11:14:49 +0200

    Fixed doc comment

--- contest_service/src/utils.rs
+++ contest_service/src/utils.rs
@@ -20,7 +20,7 @@ pub fn prost_ts_to_systime(t: protos::prost_types::Timestamp) -> std::time::Syst
 /// Convert a SystemTime to a MongoDB timestamp. Note that MongoDB sucks and stores unix timestamps in 32 bits
 pub fn systime_to_timestamp(st: std::time::SystemTime) -> mongodb::bson::Timestamp {
     let nano_duration = st.duration_since(std::time::UNIX_EPOCH).unwrap().as_nanos();
-    let seconds = (nano_duration / 1_000_000_000) as u32; // TODO This is bad, because we will have an overflow on 2032 or such
+    let seconds = (nano_duration / 1_000_000_000) as u32; // TODO This is bad, because we will have an overflow in 2106
     mongodb::bson::Timestamp {
         time: seconds,
         increment: 0,
