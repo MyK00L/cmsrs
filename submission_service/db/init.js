873a4439e936cbf01b07430023fd95d9f5659463
filippo <filippo.selvatici20@imperial.ac.uk>
2021-08-22 17:09:12 +0200

    Define db schema.

--- submission_service/db/init.js
+++ submission_service/db/init.js
@@ -1,20 +1,75 @@
-db.createCollection("contests")
-db.contests.createIndex(
-    { "users.username": 1 },
-    {
-        "name": "authUserIndex",
-        "unique": true,
-        "sparse": true
+db.createCollection("submissions", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      required: ["authorUsername", "problemId", "created", "source", "state"],
+      _id: { bsonType: "string" }, // submission id
+      properties: {
+        authorUsername: { bsonType: "string" },
+        problemId: { bsonType: "int" },
+        created: { bsonType: "timestamp" },
+        source: { bsonType: "binData" },
+        state: { enum: ['Pending','Evaluated'] },
+        compilation: {
+          bsonType: "object",
+          required: ["outcome", "timeMs", "memoryB"],
+          properties: {
+            outcome: { bsonType: "string" },
+            timeMs: { bsonType: "int" },
+            memoryB: { bsonType: "int" },
+            error: { bsonType: "string" }
+          }
+        },
+        evaluation: {
+          bsonType: "object",
+          required: ["subtasks"],
+          properties: {
+            subtasks: {
+              bsonType: "array",
+              items: {
+                bsonType: "object",
+                required: ["testcases", "subtaskScore"],
+                properties: {
+                  subtaskScore: { 
+                    oneOf: [ 
+                      { bsonType: "bool"},
+                      { bsonType: "double"}
+                    ]
+                  },
+                  testcases: {
+                    bsonType: "array",
+                    items: {
+                      bsonType: "object",
+                      required: ["testcaseOutcome", "timeMs", "memoryB"],
+                      properties: {
+                        testcaseOutcome: {
+                          bsonType: "object",
+                          required: ["verdict"],
+                          properties: {
+                              verdict: { bsonType : "string" } ,
+                              score: { bsonType: "double" },
+                          }
+                        },
+                        timeMs: { bsonType: "int" },
+                        memoryB: { bsonType: "int" },
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        },
+        overallScore: { 
+          oneOf: [ 
+            { bsonType: "bool"},
+            { bsonType: "double"}
+          ]
+        }
+      }
     }
-)
-
-db.contests.insertOne({
-    "name": "default_contest",
-    "description": "Default contest",
-    "startTime": ISODate(),
-    "endTime": ISODate(),
-    "problems": [],
-    "users": [],
-    "announcements": [],
-    "questions": []
+  }
 })
+
+// create index for field authorUsername
+db.collection.createIndex( { authorUsername: "text" } )
