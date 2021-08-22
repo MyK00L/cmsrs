eb2f0af374df633c1d0788fa3073d463a80532b0
Eugenio Tampieri <eugenio@eutampieri.eu>
2021-08-22 13:53:17 +0200

    Fixed some comments

--- convert/src/mongo.rs
+++ convert/src/mongo.rs
@@ -3,10 +3,10 @@ pub fn timestamp_to_systime(ts: bson::Timestamp) -> std::time::SystemTime {
     std::time::UNIX_EPOCH + std::time::Duration::from_secs(ts.time.into())
 }
 
-/// Convert a SystemTime to a MongoDB timestamp. Note that MongoDB sucks and stores unix timestamps in 32 bits
+/// Convert a SystemTime to a MongoDB timestamp. Note that MongoDB stores unix timestamps in 32 bits
 pub fn systime_to_timestamp(st: std::time::SystemTime) -> bson::Timestamp {
     let nano_duration = st.duration_since(std::time::UNIX_EPOCH).unwrap().as_nanos();
-    let seconds = (nano_duration / 1_000_000_000) as u32; // TODO This is bad, because we will have an overflow in 2106
+    let seconds = (nano_duration / 1_000_000_000) as u32; // This will overflow in 2106
     bson::Timestamp {
         time: seconds,
         increment: 0,
