46ce68a46411f2209211c626b9a8924836a459f9
Michael Chelli <michaelchelli00@gmail.com>
2021-08-23 16:53:21 +0200

    contest_client example and stuff (≧∇≦)b

--- admin/src/main.rs
+++ admin/src/main.rs
@@ -1,11 +1,10 @@
 use rocket::form::{Form, Strict};
-use rocket::fs::{relative, NamedFile};
 use rocket::http::{Cookie, CookieJar};
 use rocket::outcome::IntoOutcome;
 use rocket::request::FromRequest;
 use rocket::response::Redirect;
+use rocket::response::content::Html;
 use rocket::*;
-use std::path::{Path, PathBuf};
 
 const PASS: &str = "1234";
 
@@ -17,12 +16,12 @@ impl<'r> FromRequest<'r> for Admin {
         request
             .cookies()
             .get_private("admin")
-            .and_then(|_cookie| Some(Admin {}))
+            .map(|_cookie| Admin {})
             .or_forward(())
     }
 }
 
-// forms
+// API (forms)
 
 #[derive(FromForm)]
 struct LoginForm {
@@ -32,48 +31,52 @@ struct LoginForm {
 async fn login_form(cookies: &CookieJar<'_>, login: Form<Strict<LoginForm>>) -> Redirect {
     if login.pass == PASS {
         cookies.add_private(Cookie::new("admin", "admin"));
-        Redirect::to("/home.html")
+        Redirect::to(uri!(home))
     } else {
         Redirect::to(uri!(root))
     }
 }
-
-// static
-
-#[rocket::get("/<path..>",rank=6)]
-async fn static_files(_admin: Admin, path: PathBuf) -> Option<NamedFile> {
-    let mut path = Path::new(relative!("static")).join(path);
-    if path.is_dir() {
-        path.push("index.html");
-    }
-    eprintln!("serving static {:?}", path);
-    NamedFile::open(path).await.ok()
+use protos::service::contest::contest_server::Contest;
+#[get("/prova")]
+async fn prova(contest_client_a: &State<protos::service::contest::MockContest>) -> String {
+    let contest_client = contest_client_a.inner().clone();
+    let res = contest_client.auth_user(tonic::Request::new(protos::service::contest::AuthUserRequest{passw:String::from("Hi"),name:String::from("Hi")})).await;
+    format!("hi {:?}", res)
 }
-#[rocket::get("/<_path..>",rank=7)]
-async fn static_files_redirect(_admin: Admin, _path: PathBuf) -> Redirect {
-    Redirect::to(uri!(root))
-}
+
+// pages
 
 #[get("/")]
 async fn root_logged(_admin: Admin) -> Redirect {
-    Redirect::to("/home.html")
+    Redirect::to(uri!(home))
 }
 #[get("/", rank = 2)]
-async fn root() -> Option<NamedFile> {
-    let mut path = Path::new(relative!("static/login.html"));
-    NamedFile::open(path).await.ok()
+async fn root() -> Html<String> {
+    Html(format!("{}", include_str!("../www/login.html")))
+}
+
+#[get("/home")]
+async fn home(_admin: Admin) -> Html<String> {
+    Html(format!("{}", include_str!("../www/home.html")))
+}
+#[get("/home", rank = 2)]
+async fn home_redirect() -> Redirect {
+    Redirect::to(uri!(root))
 }
 
 #[launch]
 fn rocket() -> _ {
+    let contest_client = protos::service::contest::MockContest::default();
     rocket::build()
+        .manage(contest_client)
         .mount(
             "/",
             routes![
+                prova,
                 root,
                 root_logged,
-                static_files,
-                static_files_redirect,
+                home,
+                home_redirect,
                 login_form
             ],
         )
