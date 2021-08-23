0c0abc485e6f62e6a61d8c467f9c94e4b89ba33b
Michael Chelli <michaelchelli00@gmail.com>
2021-08-23 13:46:26 +0200

    pls help (#^ω^)

--- rpctest-server/src/tests.rs
+++ rpctest-server/src/tests.rs
@@ -1,6 +1,7 @@
 use super::*;
 use tokio_test::block_on;
 use tower::ServiceBuilder;
+use tonic::Request;
 
 #[test]
 fn string_test() {
@@ -34,4 +35,25 @@ fn mock_with_err() {
         "{:?}",
         block_on(t.test_string(request)).unwrap().into_inner()
     );
+}
+
+async fn async_file_test() {
+    let mut mock_test = MockTest::default();
+    let messages = vec![
+        FileRequest {file: (b"owo").to_vec(),},
+        FileRequest {file: (b"ouo").to_vec(),},
+        FileRequest {file: (b"ovo").to_vec(),},
+    ];
+    let channel = ServiceBuilder::new().service(TestServer::new(mock_test));
+    let test_client = TestClient::new(channel);
+    let t = MyTest { test_client };
+    let mut streaming_response = t.file(tonic::Request::new(futures_util::stream::iter(messages.clone()))).await.unwrap();
+    /*while let Some(res) = block_on(streaming_response.message()).unwrap() {
+        println!("{:?}", res);
+    }*/
+}
+
+#[test]
+fn file_test() {
+    block_on(async_file_test());
 }
