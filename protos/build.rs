a7aa3984b3efa7748c4766d35909de31187ca70e
Giacomo Aloisi <giacomo.aloisi1998@gmail.com>
2021-08-26 22:00:09 +0200

    Fix formatting

--- protos/build.rs
+++ protos/build.rs
@@ -1,21 +1,27 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-    .type_attribute(".service.evaluation", "#[derive(::serde::Serialize,::serde::Deserialize)]")
-    .type_attribute(".scoring", "#[derive(::serde::Serialize,::serde::Deserialize)]")
-    .compile(
-        &[
-            "protos/common.proto",
-            "protos/evaluation.proto",
-            "protos/scoring.proto",
-            "protos/worker.proto",
-            "protos/service/contest.proto",
-            "protos/service/dispatcher.proto",
-            "protos/service/evaluation.proto",
-            "protos/service/submission.proto",
-            "protos/service/worker.proto",
-            "protos/service/test.proto",
-        ],
-        &["protos"],
-    )?;
+        .type_attribute(
+            ".service.evaluation",
+            "#[derive(::serde::Serialize,::serde::Deserialize)]",
+        )
+        .type_attribute(
+            ".scoring",
+            "#[derive(::serde::Serialize,::serde::Deserialize)]",
+        )
+        .compile(
+            &[
+                "protos/common.proto",
+                "protos/evaluation.proto",
+                "protos/scoring.proto",
+                "protos/worker.proto",
+                "protos/service/contest.proto",
+                "protos/service/dispatcher.proto",
+                "protos/service/evaluation.proto",
+                "protos/service/submission.proto",
+                "protos/service/worker.proto",
+                "protos/service/test.proto",
+            ],
+            &["protos"],
+        )?;
     Ok(())
 }
