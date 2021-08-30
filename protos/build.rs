d289c08fac46dbaad5429f58c55c47ef0a589282
Michael Chelli <michaelchelli00@gmail.com>
2021-08-30 16:16:02 +0200

    fmt :p

--- protos/build.rs
+++ protos/build.rs
@@ -1,9 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .type_attribute(
-            ".",
-            "#[derive(::serde::Serialize,::serde::Deserialize)]",
-        )
+        .type_attribute(".", "#[derive(::serde::Serialize,::serde::Deserialize)]")
         .compile(
             &[
                 "protos/common.proto",
