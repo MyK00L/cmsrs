4add0f4738a9cb70b365bc99508f26dc0e020ee9
Giacomo Aloisi <giacomo.aloisi1998@gmail.com>
2021-08-31 16:09:39 +0200

    Implement add folder, search item and save file

--- utils/src/storage.rs
+++ utils/src/storage.rs
@@ -1,4 +1,5 @@
-use std::fs::DirBuilder;
+use std::fs::{DirBuilder, File};
+use std::io::Write;
 use std::path::{Path, PathBuf};
 
 pub struct FsStorageHelper {
@@ -15,10 +16,66 @@ impl FsStorageHelper {
             })
     }
 
-    pub fn add_container(self, path: &Path) -> Result<Self, std::io::Error> {
+    fn strip_to_root(&self, path: &Path) -> PathBuf {
+        path.strip_prefix(self.root.clone()).unwrap().to_path_buf()
+    }
+
+    pub fn add_folder(
+        &self,
+        folder_name: &str,
+        path: Option<&Path>,
+    ) -> Result<PathBuf, std::io::Error> {
+        let ancestor_path = match path {
+            Some(ap) => self.root.join(ap),
+            None => self.root.clone(),
+        };
+        let folder_path = ancestor_path.join(folder_name);
+        let new_path = self.strip_to_root(&folder_path);
         DirBuilder::new()
             .recursive(true)
-            .create(self.root.join(path).as_path())
-            .map(|_| self)
+            .create(folder_path)
+            .map(|_| new_path)
+    }
+
+    pub fn search_item(
+        &self,
+        folder_path: Option<&Path>,
+        item_name: &str,
+    ) -> Result<Option<PathBuf>, std::io::Error> {
+        let absolute_folder_path = match folder_path {
+            Some(ap) => self.root.join(ap),
+            None => self.root.clone(),
+        };
+        if absolute_folder_path.is_dir() {
+            return Ok(absolute_folder_path
+                .read_dir()?
+                .flat_map(|res| res.map(|e| e.path()))
+                .find(|el| {
+                    el.file_stem()
+                        .filter(|&os| os.to_str().filter(|&s| s == item_name).is_some())
+                        .is_some()
+                })
+                .map(|path| self.strip_to_root(&path)));
+        }
+        Ok(None)
+    }
+
+    pub fn save_file(
+        &self,
+        folder_path: &Path,
+        file_name: &str,
+        extension: &str,
+        content: &[u8],
+    ) -> Result<(), std::io::Error> {
+        let mut path = self.root.join(folder_path);
+        path.set_file_name(file_name);
+        path.set_extension(extension);
+        let mut buffer = File::create(path)?;
+        buffer.write_all(content)?;
+        Ok(())
+    }
+
+    pub fn read_file(&self, _path: &Path) -> Result<Vec<u8>, std::io::Error> {
+        todo!()
     }
 }
--- utils/src/tests.rs
+++ utils/src/tests.rs
@@ -56,36 +56,3 @@ fn already_init_fs_storage_helper_test() {
     let storage2 = storage::FsStorageHelper::new(test_path.as_path());
     assert!(storage2.is_ok());
 }
-
-#[test]
-fn add_storage_fs_storage_helper_test() {
-    let temp_dir = TempDir::default();
-    let test_path = temp_dir.to_path_buf().join("test");
-    let test_container_path = test_path.join("testcontaner");
-    let res = storage::FsStorageHelper::new(test_path.as_path())
-        .and_then(|store| store.add_container(test_container_path.as_path()));
-    assert!(res.is_ok());
-    assert!(test_container_path.exists());
-}
-
-#[test]
-fn add_storage_recursive_fs_storage_helper_test() {
-    let temp_dir = TempDir::default();
-    let test_path = temp_dir.to_path_buf().join("test");
-    let test_container_recursive_path = test_path.join("foo/bar/asd/testcontaner");
-    let res = storage::FsStorageHelper::new(test_path.as_path())
-        .and_then(|store| store.add_container(test_container_recursive_path.as_path()));
-    assert!(res.is_ok());
-    assert!(test_container_recursive_path.exists());
-}
-
-#[test]
-fn already_add_storage_fs_storage_helper_test() {
-    let temp_dir = TempDir::default();
-    let test_path = temp_dir.to_path_buf().join("test");
-    let test_container_path = test_path.join("testcontaner");
-    let res = storage::FsStorageHelper::new(test_path.as_path())
-        .and_then(|store| store.add_container(test_container_path.as_path()))
-        .and_then(|store| store.add_container(test_container_path.as_path()));
-    assert!(res.is_ok());
-}
