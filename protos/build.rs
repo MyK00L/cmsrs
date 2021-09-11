866b2019f7da448e6cd12221bc6ee2a297d71314
Michael Chelli <michaelchelli00@gmail.com>
2021-09-11 18:58:10 +0200

    (*ﾉω・*)

--- protos/build.rs
+++ protos/build.rs
@@ -1,7 +1,6 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .type_attribute(".", "#[derive(::serde::Serialize,::serde::Deserialize,::fake::Dummy,::strum_macros::ToString,::strum_macros::EnumIter,::strum_macros::EnumString)]") // hacky hacks
-        .type_attribute(".", "#[allow(unused_imports)]\nuse fake::*;") // more hacky hacks
+        .type_attribute(".", "#[allow(unused_imports)]\nuse fake::*;\n#[derive(::serde::Serialize,::serde::Deserialize,::fake::Dummy)]") // hacky hacks
         .compile(
             &[
                 "protos/common.proto",
