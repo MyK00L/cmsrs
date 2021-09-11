593dbda3ea4fbce89b43bf2b8a5881f87376ae41
Michael Chelli <michaelchelli00@gmail.com>
2021-09-11 18:29:40 +0200

    added useful derives to generated protos

--- protos/build.rs
+++ protos/build.rs
@@ -1,6 +1,7 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
-        .type_attribute(".", "#[derive(::serde::Serialize,::serde::Deserialize)]")
+        .type_attribute(".", "#[derive(::serde::Serialize,::serde::Deserialize,::fake::Dummy,::strum_macros::ToString,::strum_macros::EnumIter,::strum_macros::EnumString)]") // hacky hacks
+        .type_attribute(".", "#[allow(unused_imports)]\nuse fake::*;") // more hacky hacks
         .compile(
             &[
                 "protos/common.proto",
