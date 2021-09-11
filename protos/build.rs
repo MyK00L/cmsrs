f311f756fe7d3c82e090b1fec555019c123be602
Michael Chelli <michaelchelli00@gmail.com>
2021-09-11 19:37:54 +0200

    added some strum macros by hand :p

--- protos/build.rs
+++ protos/build.rs
@@ -1,6 +1,15 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     tonic_build::configure()
         .type_attribute(".", "#[allow(unused_imports)]\nuse fake::*;\n#[derive(::serde::Serialize,::serde::Deserialize,::fake::Dummy)]") // hacky hacks
+        .type_attribute("common.ProgrammingLanguage","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
+        .type_attribute("common.CompilationResult.Outcome","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
+        .type_attribute("scoring.Subtask.Method","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
+        .type_attribute("scoring.Problem.Method","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
+        .type_attribute("scoring.User.Method.Aggregation","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
+        .type_attribute("worker.SourceFile.Type","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
+        .type_attribute("service.evaluation.EvaluationFile.Type","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
+        .type_attribute("service.evaluation.Problem.Type","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
+        .type_attribute("service.submission.SubmissionState","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
         .compile(
             &[
                 "protos/common.proto",
