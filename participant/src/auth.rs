c9396cb1c3f5aa6d7ee65fb1b912f82b5d862a70
Michael Chelli <michaelchelli00@gmail.com>
2021-10-02 15:29:31 +0200

    moved login to static :<>

--- participant/src/auth.rs
+++ participant/src/auth.rs
@@ -7,7 +7,8 @@ use rocket::outcome::IntoOutcome;
 use rocket::request::FromRequest;
 use rocket::response::{status, Redirect};
 use rocket::*;
-use rocket_dyn_templates::Template;
+use rocket::fs::{relative, NamedFile};
+use std::path::{Path, PathBuf};
 
 pub struct User(pub String);
 #[rocket::async_trait]
@@ -22,24 +23,33 @@ impl<'r> FromRequest<'r> for User {
     }
 }
 
+// login page, redirects if already logged
 #[get("/")]
 pub async fn root_logged(_user: User) -> Redirect {
     // TODO redirect to main page
     Redirect::to(uri!(super::questions::questions))
 }
 #[get("/", rank = 2)]
-pub async fn root() -> Result<Template, status::Custom<()>> {
-    Ok(Template::render(
-        "login",
-        std::collections::HashMap::<String, String>::new(),
-    ))
+pub async fn root() -> Option<NamedFile> {
+    let path = Path::new(relative!("static/login/index.html"));
+    NamedFile::open(path).await.ok()
 }
 
-#[rocket::get("/<_path..>", rank = 8)]
-pub async fn not_logged_redirect(_path: std::path::PathBuf) -> Redirect {
+// can access static files other than login only if logged
+#[get("/<path..>", rank = 7)]
+pub async fn statics(_user: User, path: PathBuf) -> Option<NamedFile> {
+    let mut path = Path::new(relative!("static")).join(path);
+    if path.is_dir() {
+        path.push("index.html");
+    }
+    NamedFile::open(path).await.ok()
+}
+#[get("/<_path..>", rank = 8)]
+pub async fn not_logged_redirect(_path: PathBuf) -> Redirect {
     Redirect::to(uri!(root))
 }
 
+// login and logout apis
 #[derive(FromForm)]
 pub struct Login {
     name: String,
