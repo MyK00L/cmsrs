5fa200fa7e4be450397bba0ec23d9cdf2a81f57c
Michael Chelli <michaelchelli00@gmail.com>
2021-10-04 13:31:33 +0200

    cleaned protos build

--- protos/build.rs
+++ protos/build.rs
@@ -1,30 +1,43 @@
+const PROTO_FILES: [&str; 10] = [
+    "protos/common.proto",
+    "protos/evaluation.proto",
+    "protos/scoring.proto",
+    "protos/worker.proto",
+    "protos/service/contest.proto",
+    "protos/service/dispatcher.proto",
+    "protos/service/evaluation.proto",
+    "protos/service/submission.proto",
+    "protos/service/worker.proto",
+    "protos/service/test.proto",
+];
+
+const ENUMS: [&str; 10] = [
+    "common.ProgrammingLanguage",
+    "evaluation.CompilationResult.Outcome",
+    "evaluation.TestcaseResult.Outcome",
+    "scoring.Subtask.Method",
+    "scoring.Problem.Method",
+    "scoring.User.Method.Aggregation",
+    "worker.SourceFile.Type",
+    "service.evaluation.EvaluationFile.Type",
+    "service.evaluation.Problem.Type",
+    "service.submission.SubmissionState",
+];
+
+const ENUM_ATTRIBUTES: &str =
+    "#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]";
+const GENERAL_ATTRIBUTES: &str = "#[allow(unused_imports)]\nuse fake::*;\n#[derive(::serde::Serialize,::serde::Deserialize,::fake::Dummy)]"; // hacky hacks
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    tonic_build::configure()
-        .type_attribute(".", "#[allow(unused_imports)]\nuse fake::*;\n#[derive(::serde::Serialize,::serde::Deserialize,::fake::Dummy)]") // hacky hacks
-        .type_attribute("common.ProgrammingLanguage","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .type_attribute("evaluation.CompilationResult.Outcome","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .type_attribute("evaluation.TestcaseResult.Outcome","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .type_attribute("scoring.Subtask.Method","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .type_attribute("scoring.Problem.Method","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .type_attribute("scoring.User.Method.Aggregation","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .type_attribute("worker.SourceFile.Type","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .type_attribute("service.evaluation.EvaluationFile.Type","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .type_attribute("service.evaluation.Problem.Type","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .type_attribute("service.submission.SubmissionState","#[derive(::strum_macros::EnumString,::strum_macros::ToString,::strum_macros::EnumIter)]")
-        .compile(
-            &[
-                "protos/common.proto",
-                "protos/evaluation.proto",
-                "protos/scoring.proto",
-                "protos/worker.proto",
-                "protos/service/contest.proto",
-                "protos/service/dispatcher.proto",
-                "protos/service/evaluation.proto",
-                "protos/service/submission.proto",
-                "protos/service/worker.proto",
-                "protos/service/test.proto",
-            ],
-            &["protos"],
-        )?;
+    println!("cargo:rerun-if-changed=build.rs");
+    PROTO_FILES
+        .iter()
+        .for_each(|x| println!("cargo:rerun-if-changed={}", x));
+    let mut config = prost_build::Config::new();
+    config.type_attribute(".", &GENERAL_ATTRIBUTES);
+    ENUMS.iter().for_each(|x| {
+        config.type_attribute(x, &ENUM_ATTRIBUTES);
+    });
+    tonic_build::configure().compile_with_config(config, &PROTO_FILES, &["protos"])?;
     Ok(())
 }
