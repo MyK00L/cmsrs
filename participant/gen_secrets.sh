97864532ef97c930065067cb5be4e465496c88cc
MarcoSelvatici <marcoselvatici98@gmail.com>
2021-10-16 19:15:10 +0200

    Add Dockerfile for participant web server.
    TODO: it is, at the moment, not possible to connect to the web server from the host machine.

--- participant/gen_secrets.sh
+++ participant/gen_secrets.sh
@@ -1,7 +1,14 @@
+#!/bin/sh
 # generate tls certs
 mkdir -p certs
-openssl req -newkey rsa:2048 -nodes -keyout certs/key.pem -x509 -days 365 -out certs/certificate.pem
+openssl req -newkey rsa:2048 \
+    -nodes \
+    -keyout certs/key.pem \
+    -x509 \
+    -days 365 \
+    -out certs/certificate.pem \
+    -subj "/C=IT/ST=/L=/O=/OU=/CN=/emailAddress="
 # replace rocket secret
 secret_key="\"$(openssl rand -base64 32)\""
-sed -i "s/^secret_key.*/secret_key=${secret_key}/" Rocket.toml
+sed -i "s/^secret_key.*/secret_key=$secret_key/" Rocket.toml
 
