f540304c64b326c202983ec60513b9b4c7c12d72
MarcoSelvatici <marcoselvatici98@gmail.com>
2021-10-17 14:55:39 +0200

    Add dockerfile and compose for admin web server.

--- admin/gen_secrets.sh
+++ admin/gen_secrets.sh
@@ -1,7 +1,14 @@
-# generate tls certs
+#!/bin/bash
+# Generate tls certs.
 mkdir -p certs
-openssl req -newkey rsa:2048 -nodes -keyout certs/key.pem -x509 -days 365 -out certs/certificate.pem
-# replace rocket secret
+openssl req -newkey rsa:2048 \
+    -nodes \
+    -keyout certs/key.pem \
+    -x509 \
+    -days 365 \
+    -out certs/certificate.pem \
+    -subj "/C=IT/ST=/L=/O=/OU=/CN=/emailAddress="
+# Replace rocket secret.
 secret_key="\"$(openssl rand -base64 32)\""
-sed -i "s/^secret_key.*/secret_key=${secret_key}/" Rocket.toml
-
+# Use pipe ("|") character as sed delimiter since the base64 secret_key cannot contian such a char.
+sed -i 's|^secret_key.*|secret_key='$secret_key'|' Rocket.toml
