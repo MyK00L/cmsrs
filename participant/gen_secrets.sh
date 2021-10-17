35a70fa14ecf9a1bfc5fc2ed94e7c8d27f05691e
MarcoSelvatici <marcoselvatici98@gmail.com>
2021-10-17 14:02:08 +0200

    Fix issues with Dockerfile for participant web server.
    Now it is possible to up the container and connect to it from the host machine at localhost:8000.

--- participant/gen_secrets.sh
+++ participant/gen_secrets.sh
@@ -1,5 +1,5 @@
-#!/bin/sh
-# generate tls certs
+#!/bin/bash
+# Generate tls certs.
 mkdir -p certs
 openssl req -newkey rsa:2048 \
     -nodes \
@@ -8,6 +8,7 @@ openssl req -newkey rsa:2048 \
     -days 365 \
     -out certs/certificate.pem \
     -subj "/C=IT/ST=/L=/O=/OU=/CN=/emailAddress="
-# replace rocket secret
+# Replace rocket secret.
 secret_key="\"$(openssl rand -base64 32)\""
-perl -pi -e 's/^secret_key.*/secret_key='$secret_key'/' Rocket.toml
+# Use pipe ("|") character as sed delimiter since the base64 secret_key cannot contian such a char.
+sed -i 's|^secret_key.*|secret_key='$secret_key'|' Rocket.toml
